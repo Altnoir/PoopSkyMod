f703ffeac7c14822ffba9356fc83dfffb7afde2f
RingLo_ <63995099+lonelyicer@users.noreply.github.com>
2025-12-22 02:16:12 +0800

    fix: fluid textures didnt register

--- src/main/java/com/altnoir/poopsky/fluid/PSFluidTypes.java
+++ src/main/java/com/altnoir/poopsky/fluid/PSFluidTypes.java
@@ -1,6 +1,7 @@
 package com.altnoir.poopsky.fluid;
 
 import com.altnoir.poopsky.PoopSky;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.neoforged.neoforge.common.SoundActions;
 import net.neoforged.neoforge.fluids.FluidType;
@@ -9,28 +10,24 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.function.Supplier;
 
-public class PSFluidTypes extends FluidType {
+public class PSFluidTypes {
     public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(NeoForgeRegistries.Keys.FLUID_TYPES, PoopSky.MOD_ID);
 
-    public static final Supplier<FluidType> POOP_FLUID_TYPE = FLUID_TYPES.register("poop", () -> new FluidType(FluidType.Properties.create()
-            .descriptionId("block.poopsky.poop_liquid")
-            .fallDistanceModifier(0F)
-            .canExtinguish(true)
-            .canConvertToSource(true)
-            .supportsBoating(true)
-            .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)
-            .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
-            .density(3000)
-            .viscosity(6000)
-            .lightLevel(1)
-    ));
+    public static final ResourceLocation POOP_STILL_TEXTURE = ResourceLocation.fromNamespaceAndPath(PoopSky.MOD_ID, "block/poop_liquid");
+    public static final ResourceLocation POOP_FLOWING_TEXTURE = ResourceLocation.fromNamespaceAndPath(PoopSky.MOD_ID, "block/poop_liquid_flowing");
+    public static final ResourceLocation POOP_OVERLAY_TEXTURE = ResourceLocation.fromNamespaceAndPath(PoopSky.MOD_ID, "block/poop_liquid");
 
-    /**
-     * Default constructor.
-     *
-     * @param properties the general properties of the fluid type
-     */
-    public PSFluidTypes(Properties properties) {
-        super(properties);
-    }
+    public static final Supplier<FluidType> POOP_FLUID_TYPE = FLUID_TYPES.register("poop", 
+            () -> new FluidType(FluidType.Properties.create()
+                    .descriptionId("block.poopsky.poop_liquid")
+                    .fallDistanceModifier(0F)
+                    .canExtinguish(true)
+                    .canConvertToSource(true)
+                    .supportsBoating(true)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .density(3000)
+                    .viscosity(6000)
+                    .lightLevel(1)
+            ));
 }
