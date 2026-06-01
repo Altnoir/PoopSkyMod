77dbe01448a3b5deb736c8e5fafd195ae6e83122
Wulian233 <1055917385@qq.com>
2026-06-01 19:41:22 +0800

    fix

--- src/main/java/com/altnoir/poopsky/block/p/PoopCandleCakeBlock.java
+++ src/main/java/com/altnoir/poopsky/block/p/PoopCandleCakeBlock.java
@@ -10,15 +10,61 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CandleCakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.BlockHitResult;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+
 public class PoopCandleCakeBlock extends CandleCakeBlock {
+    private static final Map<Block, Block> VANILLA_CANDLE_CAKES = Map.ofEntries(
+            Map.entry(Blocks.CANDLE, Blocks.CANDLE_CAKE),
+            Map.entry(Blocks.WHITE_CANDLE, Blocks.WHITE_CANDLE_CAKE),
+            Map.entry(Blocks.ORANGE_CANDLE, Blocks.ORANGE_CANDLE_CAKE),
+            Map.entry(Blocks.MAGENTA_CANDLE, Blocks.MAGENTA_CANDLE_CAKE),
+            Map.entry(Blocks.LIGHT_BLUE_CANDLE, Blocks.LIGHT_BLUE_CANDLE_CAKE),
+            Map.entry(Blocks.YELLOW_CANDLE, Blocks.YELLOW_CANDLE_CAKE),
+            Map.entry(Blocks.LIME_CANDLE, Blocks.LIME_CANDLE_CAKE),
+            Map.entry(Blocks.PINK_CANDLE, Blocks.PINK_CANDLE_CAKE),
+            Map.entry(Blocks.GRAY_CANDLE, Blocks.GRAY_CANDLE_CAKE),
+            Map.entry(Blocks.LIGHT_GRAY_CANDLE, Blocks.LIGHT_GRAY_CANDLE_CAKE),
+            Map.entry(Blocks.CYAN_CANDLE, Blocks.CYAN_CANDLE_CAKE),
+            Map.entry(Blocks.PURPLE_CANDLE, Blocks.PURPLE_CANDLE_CAKE),
+            Map.entry(Blocks.BLUE_CANDLE, Blocks.BLUE_CANDLE_CAKE),
+            Map.entry(Blocks.BROWN_CANDLE, Blocks.BROWN_CANDLE_CAKE),
+            Map.entry(Blocks.GREEN_CANDLE, Blocks.GREEN_CANDLE_CAKE),
+            Map.entry(Blocks.RED_CANDLE, Blocks.RED_CANDLE_CAKE),
+            Map.entry(Blocks.BLACK_CANDLE, Blocks.BLACK_CANDLE_CAKE)
+    );
+
     public PoopCandleCakeBlock(Block candle, Properties properties) {
         super(candle, properties);
+        restoreVanillaCandleCake(candle);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void restoreVanillaCandleCake(Block candle) {
+        Block vanillaCandleCake = VANILLA_CANDLE_CAKES.get(candle);
+        if (vanillaCandleCake == null) {
+            return;
+        }
+
+        try {
+            for (Field field : CandleCakeBlock.class.getDeclaredFields()) {
+                if (Modifier.isStatic(field.getModifiers()) && Map.class.isAssignableFrom(field.getType())) {
+                    field.setAccessible(true);
+                    ((Map<Block, Block>) field.get(null)).put(candle, vanillaCandleCake);
+                    return;
+                }
+            }
+        } catch (ReflectiveOperationException exception) {
+            throw new IllegalStateException("Unable to restore vanilla candle cake mapping", exception);
+        }
     }
 
     @Override
