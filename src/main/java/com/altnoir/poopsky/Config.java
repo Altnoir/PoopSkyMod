dbdc10657da287b215498b8b97f19daf2b4936f7
Wulian233 <1055917385@qq.com>
2026-06-06 11:42:18 +0800

    保留注释

--- src/main/java/com/altnoir/poopsky/Config.java
+++ src/main/java/com/altnoir/poopsky/Config.java
@@ -16,18 +16,23 @@ public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     private static final ModConfigSpec.BooleanValue LAVA_FLUID_BLOCK = BUILDER
+            .comment("Whether to Disable the underground lava lake")
             .translation("poopsky.configuration.lavaFluid")
             .define("lavaFluid", true);
-    private static final ModConfigSpec.BooleanValue STICK_CREAFTING = BUILDER
+    private static final ModConfigSpec.BooleanValue STICK_CRAFTING = BUILDER
+            .comment("Whether to Disable the consumption of liquid when sticks crafting")
             .translation("poopsky.configuration.stickyCrafting")
             .define("stickyCrafting", false);
     private static final ModConfigSpec.BooleanValue DESPERATE_WORLD = BUILDER
+            .comment("Whether to Enable the Desperate World (Enabling it will cause the device to lag)")
             .translation("poopsky.configuration.desperateWorld")
             .define("desperateWorld", false);
     private static final ModConfigSpec.BooleanValue SET_POOPSKY_DEFAULT = BUILDER
+            .comment("Whether the dedicated server level-type default should be set to poopsky")
             .translation("poopsky.configuration.setPoopskyDefault")
             .define("setPoopskyDefault", false);
     private static final ModConfigSpec.BooleanValue VOID_NETHER_GENERATION = BUILDER
+            .comment("Whether the custom void generator should also keep the nether empty")
             .translation("poopsky.configuration.voidNetherGeneration")
             .define("voidNetherGeneration", true);
     static final ModConfigSpec SPEC = BUILDER.build();
@@ -35,7 +40,7 @@ public class Config {
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
         lavaFluid = LAVA_FLUID_BLOCK.get();
-        stickyCrafting = STICK_CREAFTING.get();
+        stickyCrafting = STICK_CRAFTING.get();
         desperateWorld = DESPERATE_WORLD.get();
         setPoopSkyDefault = SET_POOPSKY_DEFAULT.get();
         voidNetherGeneration = VOID_NETHER_GENERATION.get();
