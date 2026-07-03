a42e3b31aaece475698cd25ba3d497fc4ac7fef7
Altnoir <2474515202@qq.com>
2026-07-04 06:19:58 +0800

    2.0-pre2

--- src/main/java/com/altnoir/poopsky/compat/jei/PSJEIInfo.java
+++ src/main/java/com/altnoir/poopsky/compat/jei/PSJEIInfo.java
@@ -2,6 +2,7 @@ package com.altnoir.poopsky.compat.jei;
 
 import com.altnoir.poopsky.common.item.PFlyTypes;
 import com.altnoir.poopsky.common.item.p.FlyItem;
+import com.altnoir.poopsky.init.PBlocks;
 import com.altnoir.poopsky.init.PItems;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
@@ -10,8 +11,16 @@ import net.minecraft.network.chat.Component;
 public class PSJEIInfo {
     public static void register(IRecipeRegistration registration) {
         registration.addIngredientInfo(
+                PItems.FOLIUM_SENNAE.get(),
+                Component.translatable("jei.poopsky.folium_senna")
+        );
+        registration.addIngredientInfo(
                 PItems.SAPLING_POOP_BALL.get(),
                 Component.translatable("jei.poopsky.sapling_poop_ball")
+        );
+        registration.addIngredientInfo(
+                PItems.SEA_POOP_BALL.get(),
+                Component.translatable("jei.poopsky.sea_poop_ball")
         );
         registration.addIngredientInfo(
                 FlyItem.withType(PFlyTypes.WHITE.get()),
@@ -37,6 +46,10 @@ public class PSJEIInfo {
                 FlyItem.withType(PFlyTypes.BLUE.get()),
                 VanillaTypes.ITEM_STACK,
                 Component.translatable("jei.poopsky.fly_desc.blue")
+        );
+        registration.addIngredientInfo(
+                PBlocks.BREEDING_BOX.get(),
+                Component.translatable("jei.poopsky.breeding_box_desc")
         );
     }
 }
