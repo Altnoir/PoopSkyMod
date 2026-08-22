f63ee03df35d41e6053c9aa450f09ea6899f0a4e
Altnoir <2474515202@qq.com>
2026-08-22 11:12:33 +0800

    2.2-all+5

--- src/main/java/com/altnoir/poopsky/data/recipe/RecipeGen.java
+++ src/main/java/com/altnoir/poopsky/data/recipe/RecipeGen.java
@@ -215,9 +215,9 @@ public class RecipeGen extends RegistrateRecipeProvider implements IConditionBui
                 .unlockedBy(getItemName(Items.ECHO_SHARD), has(Items.ECHO_SHARD))
                 .save(recipeOutput);
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, PoItems.TOTEM_OF_UNPOOPING.get(), 8)
-                .pattern("S")
-                .pattern("E")
-                .pattern("D")
+                .pattern(" S ")
+                .pattern("DED")
+                .pattern(" D ")
                 .define('S', PoBlocks.SHIT)
                 .define('E', Items.ENCHANTED_GOLDEN_APPLE)
                 .define('D', PoItems.KING_OF_DRAGON_FRUIT)
